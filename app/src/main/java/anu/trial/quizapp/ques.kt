5907e8e94624a6ac33d6bccff08951bc11e2da2a
Anushree2023 <anushreethosar@gmail.com>
2024-07-19 16:51:18 +0530

    Questions with options functionality

--- app/src/main/java/anu/trial/quizapp/ques.kt
+++ app/src/main/java/anu/trial/quizapp/ques.kt
@@ -8,6 +8,21 @@ object Constants{
         val qlist= ArrayList<Questions>()
         val q1 =Questions(1," Who is the Second Field Martial of Indian Army ?",R.drawable.quizq1,
             "Sam Manekshaw","KM Cariappa","Anushree Thosar","Ameya Thosar",1)
+        qlist.add(q1)
+
+        val q2=Questions(2," Who is the First Field MArtial of Indian Army",R.drawable.quizq1,
+            "Sam Manekshaw","KM Cariappa","Anushree Thosar","Ameya Thosar",2)
+        qlist.add(q2)
+        val q3=Questions(1," Who is in Indian Air Force ?",R.drawable.quizq1,
+            "Sam Manekshaw","KM Cariappa","Anushree Thosar","Ameya Thosar",4)
+        qlist.add(q3)
+        val q4=Questions(1," Who is Amazing and most intelligent ?",R.drawable.quizq1,
+            "Sam Manekshaw","KM Cariappa","Anushree Thosar","Ameya Thosar",3)
+        qlist.add(q4)
+        return qlist
+
+
     }
+
 
 }
