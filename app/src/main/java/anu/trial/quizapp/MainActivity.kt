9b189d54966b5e03c2f2c986cf59805e17d70a68
Anushree2023 <anushreethosar@gmail.com>
2024-07-19 16:00:21 +0530

    Toasty and Questions

--- app/src/main/java/anu/trial/quizapp/MainActivity.kt
+++ app/src/main/java/anu/trial/quizapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package anu.trial.quizapp
 
+import android.content.Intent
+import android.health.connect.datatypes.units.Length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import anu.trial.quizapp.databinding.ActivityMainBinding
+
+lateinit var Binding:ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(Binding.root)
+
+
+        Binding.start.setOnClickListener()
+        {
+            if(Binding.name.text.toString().isEmpty())
+            {
+                Toast.makeText(this,"Pleast enter the text",Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                startActivity(Intent(this,MainActivity2::class.java))
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/anu/trial/quizapp/Questions.kt
@@ -0,0 +1,17 @@
+package anu.trial.quizapp
+
+data class Questions (
+    val id:Int,
+    val question:String,
+    val o1:String,
+    val o2:String,
+    val o3:String,
+    val o4:String,
+    val ans:Int,
+    val img:Int,
+
+
+)
+
+
+
